3136ded3cca619280b95d5a21068057dcc2aa4b8
IARodin <71449805+IARodin@users.noreply.github.com>
2023-08-10 19:32:44 +0700

    add test

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -2,15 +2,21 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class APITests {
     private final int unexistringPetId = 232323493;
+    Integer id = 1220;
 
     @BeforeEach
     public void setup() {
@@ -41,6 +47,7 @@ public class APITests {
     }
 
     @Test
+    @DisplayName("Пример get запроса")
     public void petNotFoundTest_BDD() {
         given().when()
                 .get(baseURI + "pet/{id}", unexistringPetId)
@@ -51,4 +58,37 @@ public class APITests {
                 .body("message", equalTo("Pet not found"));
     }
 
+    @Test
+    @DisplayName("Пример post запроса")
+    public void newPetTest() {
+        String name = "dogg";
+        String status = "sold";
+
+        Map<String, String> request = new HashMap<>();
+        request.put("id", id.toString());
+        request.put("name", name);
+        request.put("status", status);
+
+        given().contentType("application/json")
+                .body(request)
+                .when()
+                .post(baseURI + "pet/")
+                .then()
+                .log().all()
+                .assertThat()
+                .time(lessThan(3000L))
+                .statusCode(200)
+                .body("id", equalTo(id))
+                .body("name", equalTo(name))
+                .body("status", equalTo(status));
+    }
+    @Test
+    @DisplayName("Пример delete запроса после создания питомца")
+    public void checkDelTest(){
+        delete(baseURI + "pet/" + id)
+                .then().statusCode(200)
+                .log().all()
+                .assertThat()
+                .body("type", equalTo("unknown"));
+    }
 }
